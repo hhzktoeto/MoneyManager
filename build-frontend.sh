7be51784f86a6a670c9a7d78e502e5e12c5e24a1
Dmitriy Goncharov <ritm.dgoncharov@gmail.com>
2025-05-22 14:39:53 +0300

    CHORE: updated configs & scripts

--- build-frontend.sh
+++ build-frontend.sh
@@ -2,10 +2,8 @@
 cd ./frontend || exit
 rm -rf ./dist
 npm install
-ng build --configuration production
+ng build --configuration prod
 cd ..
 mkdir -p gateway/src/main/resources/static
-mkdir -p gateway/src/main/resources/static/assets
 cp frontend/dist/frontend-angular/browser/* gateway/src/main/resources/static
 cp frontend/dist/frontend-angular/* gateway/src/main/resources/static
-cp -r frontend/src/assets/* gateway/src/main/resources/static/assets
